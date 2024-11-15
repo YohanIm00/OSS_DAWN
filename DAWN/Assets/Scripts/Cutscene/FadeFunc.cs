a653bbf8926f1138689b3f260b23b7e35787ef82
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2024-11-15 16:26:34 +0900

    chore: modify trivial features #6
    - Modifying trivial features of scenes, source code, etc.

--- DAWN/Assets/Scripts/Cutscene/FadeFunc.cs
+++ DAWN/Assets/Scripts/Cutscene/FadeFunc.cs
@@ -5,33 +5,24 @@ using DG.Tweening;
 
 public class FadeFunc : MonoBehaviour
 {
-    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private SpriteRenderer spriteRenderer;
     private Tween fadeTween;
 
-    public void FadeInsert(float duration)
+    void OnEnable()
     {
-        Fade(1f, duration, () => 
-        {
-            canvasGroup.interactable = true;
-            canvasGroup.blocksRaycasts = true;
-        });
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        Color temp = spriteRenderer.color;
+        temp.a = 0f;
+        spriteRenderer.color = temp;
+        StartCoroutine(FadeIn());
+    }
+    private IEnumerator FadeIn()
+    {
+        yield return spriteRenderer.DOFade(1,1f).WaitForCompletion();
     }
 
-    // public void FadeOut()    // Activate later when it would be used.
+    // private IEnumerator FadeIn() // Activate later when it would be used.
     // {
-    //     Fade(1f, duration, () => 
-    //     {
-    //         canvasGroup.interactable = false;
-    //         canvasGroup.blocksRaycasts = false;
-    //     });
+    //     yield return spriteRenderer.DOFade(0,1f).WaitForCompletion();
     // }
-
-    private void Fade(float endValue, float duration, TweenCallback onEnd)
-    {
-        if (fadeTween != null)
-            fadeTween.Kill(false);
-
-            fadeTween = canvasGroup.DOFade(endValue, duration);
-            fadeTween.onComplete += onEnd; 
-    }
 }
