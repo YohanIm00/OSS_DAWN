13e378b76c6b0ef808a483f8d1d6c23f1d5aadd8
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2024-11-19 17:37:40 +0900

    feat: create necessary source code of Chef #5
    - Creating several C# source codes: InteractableIF, NPC, and Chef

---
+++ DAWN/Assets/Scripts/Actors/Chef/Chef.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chef : NPC
+{
+    // Start is called before the first frame update
+    public override void Interact()
+    {
+
+    }
+
+    protected override void _Update()
+    {
+
+    }
+}
