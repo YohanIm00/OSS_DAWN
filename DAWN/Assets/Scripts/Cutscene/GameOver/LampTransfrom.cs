ac1a0c27ada2da0e7df3c5347e79489a2ae0dc88
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2024-12-01 13:35:37 +0900

    feat: implement C# source codes #10
    - Implementing new scripts with using DOTween functions

---
+++ DAWN/Assets/Scripts/Cutscene/GameOver/LampTransfrom.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class LampTransform : AbstractParts
+{
+    protected override IEnumerator Alter()
+    {
+        // yield return image.DOFade(1f, 0f);
+        yield return image.transform.DOMoveY(972, 3f, true);
+    }
+}
---
+++ DAWN/Assets/Scripts/Cutscene/GameOver/LetterTransform.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class LetterTransform : AbstractParts
+{
+    protected override IEnumerator Alter()
+    {
+        Transform transform = image.transform;
+        yield return transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0), 0.5f, 10, 1f);
+    }
+}
