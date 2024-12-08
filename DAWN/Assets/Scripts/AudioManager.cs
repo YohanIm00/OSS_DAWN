1225a3aa4ba35fe1acc734eec89a84ebfda10207
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2024-12-08 18:38:28 +0900

    feat: create AudioManager.cs
    - Creating AudioManager.cs
    - Implementing the skeletone of it

---
+++ DAWN/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("#BGM")]
+    public AudioClip[] bgmClips;
+    public float bgmVolume;
+    AudioSource bgmPlayer;
+
+    [Header("#SFX")]
+    public AudioClip[] sfxClips;
+    public float sfxVolume;
+    public int channels;
+    AudioSource[] sfxPlayers;
+    int channelIndex;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    
+}
